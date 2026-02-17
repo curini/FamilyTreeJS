31b90a4afbe0cec49c8acb1e0dd0a6e70c6829de
LAPTOP-J9U9GBB8\curin <curini>
2026-02-17 15:55:04 +0100

    update stats page

--- src/app/features/persons/pages/stats/stats.ts
+++ src/app/features/persons/pages/stats/stats.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
+import { Map } from '../../components/map/map';
+import { myStats } from '../../constants/my-stats';
+import { myTitles } from '../../constants/my-titles';
+import { PersonKeys } from '../../types/stat';
 
 @Component({
   selector: 'app-stats',
-  imports: [],
+  imports: [Map, KeyValuePipe],
   templateUrl: './stats.html',
   styleUrl: './stats.css',
 })
 export class Stats {
-
+  stats = myStats;
+  titles = myTitles;
 }
