db8e6e325e00163539808e251bb8190992ae82b5
LAPTOP-J9U9GBB8\curin <curini>
2026-02-17 15:54:26 +0100

    add constant titles

---
+++ src/app/features/persons/constants/my-titles.ts
@@ -0,0 +1,9 @@
+export const myTitles: { [key: string]: string } = {
+  nb_persons: 'Nombre de personnes',
+  nb_family_name: 'Nombre de noms de famille',
+  most_used_family_name: 'Nom de famille le plus utilisé',
+  more_younger_deceased: 'Personne la plus jeune décédée',
+  more_older_deceased: 'Personne la plus âgée décédée',
+  more_younger: 'Personne la plus jeune',
+  more_older: 'Personne la plus âgée',
+};
