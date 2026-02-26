62bf30f34163c7eed3eafaa47034d3c68cd7eeed
LAPTOP-J9U9GBB8\curin <curini>
2026-02-26 17:23:29 +0100

    add default values

---
+++ src/app/features/persons/constants/my-stats.ts
@@ -0,0 +1,14 @@
+import { Stat } from '../types/stat';
+
+export const myStats: Stat = {
+  person: {
+    nb_persons: { value: 2, color: 'text-red-800' },
+    nb_family_name: { value: 1, color: 'text-gray-700' },
+    most_used_family_name: { value: 'TOTO', color: 'text-green-600' },
+    more_younger_deceased: { value: 2, color: 'text-purple-600' },
+    more_older_deceased: { value: 93, color: 'text-blue-600' },
+    more_younger: { value: 1, color: 'text-indigo-600' },
+    more_older: { value: 86, color: 'text-orange-600' },
+  },
+  markers: [],
+};
---
+++ src/app/features/persons/constants/my-tree.ts
@@ -0,0 +1,20 @@
+export const myTree = [
+  {
+    id: 1,
+    name: 'Grand-p\u00e8re ',
+    gender: 'male',
+    img: '\/img\/portraits\/man.jpg',
+    tags: ['main_male_child'],
+    map: '\/persons\/1',
+    pids: [2],
+  },
+  {
+    id: 2,
+    name: 'Grand-m\u00e8re ',
+    gender: 'female',
+    img: '\/img\/portraits\/woman.jpg',
+    tags: ['main_female_child'],
+    map: '\/persons\/2',
+    pids: [1],
+  },
+];
