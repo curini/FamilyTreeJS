b26427ba5acaca858db443627561a8eedbd71099
LAPTOP-J9U9GBB8\curin <curini>
2026-04-15 13:24:32 +0200

    update route

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { TreeComponent } from './tree/tree.component';
 import { EditComponent } from './edit/edit.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: ListComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'tree', component: TreeComponent },
   { path: 'edit/:id', component: EditComponent },
   { path: 'create', component: EditComponent },
-  { path: '**', redirectTo: '' },
+  { path: '404', component: ErrorComponent },
+  { path: '**', redirectTo: '404' },
 ];
