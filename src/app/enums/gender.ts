26cf148a0e835115af87be9fb3afb48745445f4d
LAPTOP-J9U9GBB8\curin <curini>
2026-04-15 12:28:44 +0200

    remove enum gender

--- src/app/enums/gender.ts
+++
@@ -1,4 +0,0 @@
-export const enum Gender {
-	male = 'M',
-	female = 'F',
-}
--- src/app/interfaces/node.ts
+++ src/app/interfaces/node.ts
@@ -1,103 +1,101 @@
-import { Gender } from '../enums/gender';
-
 export interface Node {
-	/**
-	 * id person
-	 * @type {number}
-	 */
-	key: number;
-	/**
-	 * name person
-	 * @type {string}
-	 */
-	name?: string;
-	/**
-	 * photo url link
-	 * @type {string}
-	 */
-	photo?: string;
-	/**
-	 * gender person
-	 * @type {string}
-	 */
-	gender?: Gender.female | Gender.male;
-	/**
-	 * belongs group
-	 * @type {[type]}
-	 */
-	isGroup?: boolean;
-	/**
-	 * group parent person
-	 * @type {number}
-	 */
-	group?: number;
-	/**
-	 * spouse person
-	 * @type {number}
-	 */
-	spouse?: number;
-	/**
-	 * parent person
-	 * @type {string}
-	 */
-	parent?: number;
-	/**
-	 * mother person
-	 * @type {string}
-	 */
-	mother?: number;
-	/**
-	 * father person
-	 * @type {string}
-	 */
-	father?: number;
-	/**
-	 * birthday person
-	 * @type {string}
-	 */
-	birth?: string;
-	/**
-	 * deathday person
-	 * @type {string}
-	 */
-	death?: string;
-	/**
-	 * note person
-	 * @type {string}
-	 */
-	note?: string;
-	/**
-	 * colors person
-	 */
-	a?: Array<string>;
-	/**
-	 * age person
-	 * @type {string}
-	 */
-	age?: string;
-	/**
-	 * birthplace person
-	 * @type {string}
-	 */
-	birthplace?: string;
-	/**
-	* birthcountry person
-	* @type {string}
-	*/
-	birthcountry?: string;
-	/**
-	 * deathplace person
-	 * @type {string}
-	 */
-	deathplace?: string;
-	/**
-	* deathcountry person
-	* @type {string}
-	*/
-	deathcountry?: string;
-	/**
-	 * generation person
-	 * @type {number}
-	 */
-	generation?: number;
+  /**
+   * id person
+   * @type {number}
+   */
+  key: number;
+  /**
+   * name person
+   * @type {string}
+   */
+  name?: string;
+  /**
+   * photo url link
+   * @type {string}
+   */
+  photo?: string;
+  /**
+   * gender person
+   * @type {string}
+   */
+  gender?: 'M' | 'F';
+  /**
+   * belongs group
+   * @type {[type]}
+   */
+  isGroup?: boolean;
+  /**
+   * group parent person
+   * @type {number}
+   */
+  group?: number;
+  /**
+   * spouse person
+   * @type {number}
+   */
+  spouse?: number;
+  /**
+   * parent person
+   * @type {string}
+   */
+  parent?: number;
+  /**
+   * mother person
+   * @type {string}
+   */
+  mother?: number;
+  /**
+   * father person
+   * @type {string}
+   */
+  father?: number;
+  /**
+   * birthday person
+   * @type {string}
+   */
+  birth?: string;
+  /**
+   * deathday person
+   * @type {string}
+   */
+  death?: string;
+  /**
+   * note person
+   * @type {string}
+   */
+  note?: string;
+  /**
+   * colors person
+   */
+  a?: Array<string>;
+  /**
+   * age person
+   * @type {string}
+   */
+  age?: string;
+  /**
+   * birthplace person
+   * @type {string}
+   */
+  birthplace?: string;
+  /**
+   * birthcountry person
+   * @type {string}
+   */
+  birthcountry?: string;
+  /**
+   * deathplace person
+   * @type {string}
+   */
+  deathplace?: string;
+  /**
+   * deathcountry person
+   * @type {string}
+   */
+  deathcountry?: string;
+  /**
+   * generation person
+   * @type {number}
+   */
+  generation?: number;
 }
