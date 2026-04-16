f62dfc16c65184513c563092cb3828a793b2d3ea
LAPTOP-J9U9GBB8\curin <curini>
2026-04-16 12:13:41 +0200

    add guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  RedirectCommand,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (authService.isAuthenticated()) {
+    return true;
+  }
+  const loginPath = router.parseUrl('/login');
+  return new RedirectCommand(loginPath, {
+    skipLocationChange: true,
+  });
+};
